224d93deb9f222ff7f98f4fcea9d18c0319b3b9d
Red3ffect <red3ffect117@gmail.com>
2021-04-04 18:41:38 -0700

    added password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,46 @@
 // Assignment code here
-// window.alert("Please answer the following questions on the criteria of your password.");
 
-var charLength = window.prompt("How many characters should your password be?");
-charLength = parseInt(charLength);
+function getCriteria() {
+  window.alert("Hello! Welcome to Password Generator!")
+  window.alert("Please answer the following questions on the criteria of your password.");
 
-function generatePassword(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  var charLength = window.prompt("How many characters should your password be? Please choose between 8 and 128");
+
+  var index = window.prompt("Choose from the following for password criteria: Enter 1 for only uppercase letters, Enter 2 for only lowercase letters, Enter 3 for uppercase and numbers (0-9), Enter 4 for lowercase letters and numbers (0-9), Enter 5 for uppercase & lowercase & numbers; Enter 6 for Upper, lower, numbers, & special characters.");
+
+  charLength = parseInt(charLength);
+  index = parseInt(index);
+  index--;
+
+  writePassword(charLength, index);
+  return;
+}
+
+function generatePassword(length, index) {
+  var result = '';
+  var characters = [{ criteria: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' },
+  { criteria: 'abcdefghijklmnopqrstuvwxyz' },
+  { criteria: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' },
+  { criteria: 'abcdefghijklmnopqrstuvwxyz0123456789' },
+  { criteria: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' },
+  { criteria: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%' }];
+  var charactersLength = characters[index].criteria.length;
+  for (var i = 0; i < length; i++) {
+    result += characters[index].criteria.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-
-console.log(generatePassword(charLength));
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(charLength);
+function writePassword(charLength, index) {
+  var password = generatePassword(charLength, index);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getCriteria);
