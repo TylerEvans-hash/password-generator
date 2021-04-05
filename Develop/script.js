02bfc4580e82623a8f0493449e395edba793ceb0
Red3ffect <red3ffect117@gmail.com>
2021-04-04 18:51:33 -0700

    Added criteria validation

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,21 @@ function getCriteria() {
   window.alert("Please answer the following questions on the criteria of your password.");
 
   var charLength = window.prompt("How many characters should your password be? Please choose between 8 and 128");
+  charLength = parseInt(charLength);
+
+  if ( charLength === "" || charLength === null || charLength < 8 || charLength > 128 ) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return getCriteria();
+  }
 
   var index = window.prompt("Choose from the following for password criteria: Enter 1 for only uppercase letters, Enter 2 for only lowercase letters, Enter 3 for uppercase and numbers (0-9), Enter 4 for lowercase letters and numbers (0-9), Enter 5 for uppercase & lowercase & numbers; Enter 6 for Upper, lower, numbers, & special characters.");
-
-  charLength = parseInt(charLength);
   index = parseInt(index);
+
+  if ( index === "" || index === null || index < 1 || index > 6 ) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return getCriteria();
+  }
+
   index--;
 
   writePassword(charLength, index);
